b5906490b87b4202556d19dfde753d7dac758cb9
lucasvasquez07 <cvasquez@fi.uba.ar>
2024-07-07 21:34:54 -0300

    Creacion de la seccion search y su archivo js y css, Modificacion al readme

--- Frontend/js/search.js
+++ Frontend/js/search.js
@@ -1,19 +1,20 @@
-import { get_by_category } from "./metodos_juegos.js"
+import { get_by_search } from "./metodos_juegos.js"
 import { create_template_card_game } from "./templates.js"
 
-async function get_category_game() {
+async function get_search() {
     const params = new URLSearchParams(window.location.search);
-    const categoria = params.get("categoria");
-    const videojuegos = await get_by_category()
-    const new_category_games = videojuegos.filter(juego => juego.genero.split(",").find(genero => genero == categoria))
-    return new_category_games
+    const search = params.get("search");
+    const videojuegos = await get_by_search()
+    console.log(videojuegos);
+    const search_games = videojuegos.filter(juego => juego.nombre.toLowerCase().includes(search.toLowerCase()))
+    return search_games
 }
-
 window.addEventListener("load", async () => {
     setTimeout(async () => {
         const row_games = document.getElementById("list_games_home")
         row_games.innerHTML = ""
-        const category = await get_category_game()
+        const category = await get_search()
+        console.log(category);
         category.forEach((dict_juego) => {
             row_games.appendChild(create_template_card_game(dict_juego))
         })
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # E-commerce-Mayonesa-Game
 
-## Install
+## Front-end
+
+## Back-end
+
+### Install
 
 ```bash
 python3 -m venv venv
@@ -8,10 +12,16 @@ source venv/bin/activate
 pip install flask
 ```
 
-## Run
+### Run
 
 ```bash
 source venv/bin/activate
 cd backend
 flask run --debug
 ```
+
+## Autores ✒️
+
+- Almiron Leonardo - Trabajo Cooparticipacion Back-end y Crud de DB - [leonardo256](https://github.com/leonardo256)
+- Vegas Gustavo - Trabajo Cooparticipacion Back-end y Crud de DB - [gustavopzvs](https://github.com/gustavopzvs)
+- Vasquez Lucas - Trabajo Front-end - [lucasvasquez07](https://github.com/lucasvasquez07)
