abb914c4b179c4f325bac4eec58e6d707dffa4c8
Leonardo <leoalmiron786@gmail.com>
2024-07-12 23:39:57 -0300

    Se agregaron las funciones para los siguientes enpoints: get user by id, post user sign in, put user update.

--- Frontend/js/metodos_backend.js
+++ Frontend/js/metodos_backend.js
@@ -1,34 +1,98 @@
 
 export function get_all_games() {
-    const url = "../juegos.json";
+    const url = "http://localhost:5000/juegos";
     return fetch(url)
-        .then(response => response.json().then(data => data))
+        .then(response => response.json())
         .catch(err => console.log(err))
 }
 
 export function get_by_category(categoria) {
-    const url = "../juegos.json?categoria=" + categoria;
+    const url = "http://localhost:5000/juegos/categoria/" + categoria;
     return fetch(url)
-        .then(response => response.json().then(data => data))
+        .then(response => response.json())
         .catch(err => console.log(err))
 }
 
 export function get_by_id_game(id_game) {
-    const url = "../juegos.json?id_game=" + id_game;
+    const url = "http://localhost:5000/juegos/id/" + id_game;
     return fetch(url)
-        .then(response => response.json().then(data => data))
-        .catch(err => console.log(err))
-}
-export function get_by_search(name) {
-    const url = "../juegos.json?name=" + name;
-    return fetch(url)
-        .then(response => response.json().then(data => data))
+        .then(response => response.json())
         .catch(err => console.log(err))
 }
 
-export function get_by_email(email) {
-    const url = "../usuarios.json?email=" + email;
+export function get_by_search(name) {
+    const url = "http://localhost:5000/juegos/search";
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name })
+    })
+    .then(response => response.json())
+    .catch(err => console.log(err));
+}
+
+export function get_by_email(email, password) {
+    const url = "http://localhost:5000/usuario/log_in";
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email, password: password })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .catch(err => console.log('Fetch error: ', err));
+}
+
+
+export function get_user_by_id(id) {
+    const url = "http://localhost:5000/usuarios/id/" + id;
     return fetch(url)
-        .then(response => response.json().then(data => data))
-        .catch(err => console.log(err))
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .catch(err => console.log('Fetch error: ', err));
+}
+
+ 
+export function post_user_sign_in(email, password, name) {
+    const url = "http://localhost:5000/sign_in";
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email, password: password, name: name })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .catch(err => console.log('Fetch error: ', err));
+}
+
+
+export function put_user_update(id, data) {
+    const url = "http://localhost:5000/data_user/" + id;
+    return fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .catch(err => console.log(err));
 }
